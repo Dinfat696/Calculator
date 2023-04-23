cc959e9db5dbc649089830de8c5f7805568ca0f3
dinarfathrahmanov <dinfat@mail.ru>
2023-04-23 11:05:11 +0300

    готово

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -1,13 +1,42 @@
 package pro.sky.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
-    @GetMapping(path = "/calculator")
-    public String calculator() {
-        return "Добро пожаловать в калькулятор.";
+    private final  CalculatorService calculator;
+    public CalculatorController(CalculatorService calculator){
+        this.calculator=calculator;
+    }
+    @GetMapping(path = "/")
+    public String welcome() {
+        return "Добро пожаловать в калькулятор";
     }
 
+    @GetMapping(path = "/plus")
+    public String add(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    @GetMapping(path = "/minus")
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    @GetMapping(path = "/multiply")
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+
+    @GetMapping(path = "/divide")
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
+        if(num2==0){
+            return "Делить на 0 нельзя";
+        }
+        return num1 + " / " + num2 + " = " + (num1 /(double)num2);
+    }
 }
---
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -0,0 +1,9 @@
+package pro.sky.calculator;
+
+public interface CalculatorService {
+    int add(int num1, int num2);
+    int minus(int num1, int num2);
+    int multiply(int num1, int num2);
+    int divide(int num1, int num2);
+
+}
